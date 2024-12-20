e1059a776346b3773424668b26d89707a165177d
leein1 <inwon.private@gmail.com>
2024-12-21 03:18:49 +0900

    생성 - 애플리케이션 프로퍼티 파일 사이즈 제한 생성 생성 - FileController v1

--- src/main/java/com/soundbrew/soundbrew/controller/FileController.java
+++ src/main/java/com/soundbrew/soundbrew/controller/FileController.java
@@ -4,14 +4,17 @@ import com.soundbrew.soundbrew.dto.ResponseDTO;
 import com.soundbrew.soundbrew.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 @RestController
@@ -20,50 +23,127 @@ import java.nio.file.Path;
 @RequiredArgsConstructor
 public class FileController {
 
+    // 토큰 유효성 확인
+    // 범위 요청
+    //
+
 
     private final FileService fileService;
 
+
+    //
     @PostMapping(value = "/upload",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> uploadFile(
-            @RequestParam("file") MultipartFile file
-            ,@RequestParam("title") String title) {
+    public ResponseEntity<String> uploadFile(// 추후 토큰 사용
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("title") String title) {
 
         try{
 
             String uniqueFilename = fileService.uploadFile(file,title);
             return ResponseEntity.ok("성공" + uniqueFilename);
 
+        } catch(IllegalArgumentException e){
+
+            return ResponseEntity.badRequest().body(e.getMessage());
         } catch (IOException e) {
 
             return ResponseEntity.status(500).body("실패");
         }
-
-
     }
+
+//    @GetMapping("/download/{filename}")
+//    public ResponseEntity<Resource> downloadFile(@PathVariable String filename){    // 추후 토큰 사용
+//
+//        /*
+//        토큰 검증 과정 필요함!!!!
+//
+//
+//         */
+//
+//
+//        try {
+//            Path filePath = fileService.downloadFile(filename);
+//
+//            //  파일 경로로 리소스 객체 생성
+//            //  파일 데이터를 HTTP 응답에 첨부 가능하게 해줌
+//            Resource resource = new UrlResource(filePath.toUri());
+//
+//            //  파일 없는 경우
+//            if (!resource.exists()) {
+//                return ResponseEntity.notFound().build();
+//            }
+//
+//            return ResponseEntity.ok()
+//                    .header("Content-Disposition", "attachment; filename=\"" + filename + "\"")
+//                    .body(resource);
+//        } catch (IOException e) {
+//            return ResponseEntity.status(500).build();
+//        }
+//
+//    }
 
     @GetMapping("/download/{filename}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String filename){
+    public ResponseEntity<Resource> downloadFile(
+            @PathVariable String filename) throws IOException {// 추후 토큰 사용
 
         try {
+            Resource resource = fileService.downloadFile(filename);
             Path filePath = fileService.getFile(filename);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, Files.probeContentType(filePath))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                    .body(resource);
+        } catch (SecurityException e) {
+            return ResponseEntity.status(401).body(null);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body(null);
+        }
+    }
 
-            //  파일 경로로 리소스 객체 생성
-            //  파일 데이터를 HTTP 응답에 첨부 가능하게 해줌
-            Resource resource = new UrlResource(filePath.toUri());
+    @GetMapping("/stream/{filename}")
+    public ResponseEntity<Resource> streamFile(
+            @PathVariable String filename,
+            @RequestParam(value = "token", required = false) String token) throws IOException {
 
-            //  파일 없는 경우
-            if (!resource.exists()) {
-                return ResponseEntity.notFound().build();
+        try {
+
+            Path filePath = fileService.getFile(filename);
+            long fileSize = Files.size(filePath);
+
+            // 범위 ㅈㅔ한
+            long maxAllowedRange = 5 * 1024 * 1024; // 5MB
+
+            // 토큰이 없는 경우 시작부터 1분까지만 - 320kbps
+            if (token == null) {
+
+                long allowedEnd = 60 * 40 * 1024; // 1분 (320kbps)
+
+                byte[] data = fileService.readPartialFile(filePath, 0, Math.min(fileSize - 1, allowedEnd));
+
+                return ResponseEntity.status(206)
+                        .header(HttpHeaders.CONTENT_TYPE, Files.probeContentType(filePath))
+                        .header(HttpHeaders.CONTENT_RANGE, "bytes 0-" + allowedEnd + "/" + fileSize)
+                        .body(new ByteArrayResource(data));
             }
 
-            return ResponseEntity.ok()
-                    .header("Content-Disposition", "attachment; filename=\"" + filename + "\"")
-                    .body(resource);
-        } catch (IOException e) {
-            return ResponseEntity.status(500).build();
-        }
+            // 토큰 검증 현재는 그냥 문자열로 "user"가 아니면 오류 반환
+            fileService.validateToken(token);
 
+            // 제한범위 내 전체 파일 전송
+            byte[] data = fileService.readPartialFile(filePath, 0, Math.min(fileSize - 1, maxAllowedRange));
+
+            return ResponseEntity.status(206)
+                    .header(HttpHeaders.CONTENT_TYPE, Files.probeContentType(filePath))
+                    .header(HttpHeaders.CONTENT_RANGE, "bytes 0-" + (Math.min(fileSize - 1, maxAllowedRange)) + "/" + fileSize)
+                    .body(new ByteArrayResource(data));
+
+        } catch (SecurityException e) {
+            return ResponseEntity.status(401).body(null);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body(null);
+        }
     }
+
 
 
 
--- src/main/java/com/soundbrew/soundbrew/service/FileService.java
+++ src/main/java/com/soundbrew/soundbrew/service/FileService.java
@@ -5,55 +5,142 @@ import com.soundbrew.soundbrew.repository.MusicFileRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.UUID;
+import org.springframework.core.io.Resource;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class FileService {
 
-    //  필드 이름 최대한 관례를 참고하였음
+    /*
+    DB update는 생략 나중에 추가해야 함
+     */
 
     private final FileProperties fileProperties;
-
     private final MusicFileRepository repository;
 
+    private final Path uploadDir = Paths.get(System.getProperty("user.dir"), "upload");
 
-    //  업로드 (테스트 용이라 mp3,wav 확장자 지원)
+
+    //  업로드 (mp3,wav 확장자만 지원)
     public String uploadFile(MultipartFile file,String title) throws IOException {
 
-        String uploadDir = fileProperties.getUploadDir();
-        Path uploadPath = Paths.get(System.getProperty("user.dir"), uploadDir);
+        //  확장자 검증
+        String filename = file.getOriginalFilename();
+        String extension = filename.substring(filename.lastIndexOf("."));
 
-        //  디렉토리 없으면 생성
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
+
+        //  null이거나 , !(mp3 or wav) 인 경우
+        if(filename == null || !(extension.equals(".mp3") || extension.equals(".wav"))){
+            throw new IllegalArgumentException(
+                    "지원하지 않는 파일 형식." + filename.substring((filename.lastIndexOf(".")))
+            );
         }
 
+        //  디렉토리 없으면 생성
+        if (!Files.exists(uploadDir)) {
+            Files.createDirectories(uploadDir);
+        }
 
-        String originalFilename = file.getOriginalFilename();
-        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
         String uniqueFilename = UUID.randomUUID().toString() + extension;
 
         //  저장
-        Path destinationPath = uploadPath.resolve(uniqueFilename);
+        Path destinationPath = uploadDir.resolve(uniqueFilename);
         Files.copy(file.getInputStream(), destinationPath);
 
         return uniqueFilename;
+
+//        String uploadDir = fileProperties.getUploadDir();
+//        Path uploadPath = Paths.get(System.getProperty("user.dir"), uploadDir);
+//
+//        //  경로 없으면 생성
+//        if (!Files.exists(uploadPath)) {
+//            Files.createDirectories(uploadPath);
+//        }
+//
+//
+//        String originalFilename = file.getOriginalFilename();
+//        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+//        String uniqueFilename = UUID.randomUUID().toString() + extension;
+//
+//        //  저장
+//        Path destinationPath = uploadPath.resolve(uniqueFilename);
+//        Files.copy(file.getInputStream(), destinationPath);
+//
+//        return uniqueFilename;
     }
 
-    //  파일 가져오기
+    //  파일 다운로드
+    public Resource downloadFile(String filename) throws IOException { // 토큰 인증 해야 함!!!!
+
+    /*
+    토큰 인증 필요
+     */
+
+        Path filePath = uploadDir.resolve(filename);
+
+        return new ByteArrayResource(Files.readAllBytes(filePath));
+
+//        String uploadDir = fileProperties.getUploadDir();
+//
+//        return Paths.get(System.getProperty("user.dir"), uploadDir, filename);
+    }
+
+    public Resource streamFile(String filename, String rangeHeader) throws IOException {    // 토큰 인증 해야 함!!!!
+        /*
+        토큰 인증 필요
+         */
+
+        Path filePath = uploadDir.resolve(filename);
+
+        if (rangeHeader != null) {
+
+            String[] ranges = rangeHeader.replace("bytes=", "").split("-");
+            long start = Long.parseLong(ranges[0]);
+            long end = ranges.length > 1 ? Long.parseLong(ranges[1]) : Files.size(filePath) - 1;
+
+            if (end >= Files.size(filePath)) end = Files.size(filePath) - 1;
+
+            byte[] data = readPartialFile(filePath, start, end);
+            return new ByteArrayResource(data);
+        }
+
+        return new ByteArrayResource(Files.readAllBytes(filePath));
+    }
+
+
+    public byte[] readPartialFile(Path filePath, long start, long end) throws IOException {
+        try (RandomAccessFile file = new RandomAccessFile(filePath.toFile(), "r")) {
+            file.seek(start);
+            byte[] data = new byte[(int) (end - start + 1)];
+            file.readFully(data);
+            return data;
+        }
+    }
+
+    public void validateToken(String token) {
+
+        if (!token.equals("user")) {
+            throw new SecurityException("유효하지 않은 토큰입니다.");
+        }
+    }
+
     public Path getFile(String filename) {
-
-        String uploadDir = fileProperties.getUploadDir();
-
-        return Paths.get(System.getProperty("user.dir"), uploadDir, filename);
+        return uploadDir.resolve(filename);
     }
+
+
+
 }
