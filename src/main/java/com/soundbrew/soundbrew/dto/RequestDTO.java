5cf135c72a86e45392799debd1c390c4c870d0bb
leein1 <inwon.private@gmail.com>
2024-12-28 17:40:29 +0900

    /DTO

--- src/main/java/com/soundbrew/soundbrew/dto/RequestDTO.java
+++ src/main/java/com/soundbrew/soundbrew/dto/RequestDTO.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.Sort;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Map;
 
@@ -44,33 +43,12 @@ public class RequestDTO {
 
     public String[] getTypes(){
         if(type == null || type.isEmpty()){
-=======
-import java.util.Map;
-
-@Data
-@SuperBuilder
-@AllArgsConstructor
-@RequiredArgsConstructor
-public class RequestDTO {
-    @Builder.Default
-    private int page=1;
-    @Builder.Default
-    private int size =10;
-    private String type;// n,t,c
-    private String keyword;
-    private String link;
-    private Map<String,String> more;
-
-    public String[] getType(){
-        if (type==null || type.isEmpty()){
->>>>>>> feature/kyoung
             return null;
         }
         return type.split("");
     }
 
     public Pageable getPageable(String... props){
-<<<<<<< HEAD
         return PageRequest.of(this.page -1, this.size, Sort.by(props).descending());
     }
 
@@ -82,15 +60,6 @@ public class RequestDTO {
 
             builder.append("page=" + this.page);
 
-=======
-        return PageRequest.of(this.page-1, this.size, Sort.by(props).descending());
-    }
-
-    public String getLink() {
-        if (link == null) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("page=" + this.page);
->>>>>>> feature/kyoung
             builder.append("&size=" + this.size);
 
             if (type != null && type.length() > 0) {
@@ -104,20 +73,6 @@ public class RequestDTO {
                     e.printStackTrace();
                 }
             }
-<<<<<<< HEAD
-=======
-
-//            if (more != null && !more.isEmpty()) {
-//                more.forEach((key, value) -> {
-//                    try {
-//                        builder.append("&more%5B" + URLEncoder.encode(key, "UTF-8") + "%5D=" + URLEncoder.encode(value, "UTF-8"));
-//                    } catch (UnsupportedEncodingException e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-//            }
-
->>>>>>> feature/kyoung
             link = builder.toString();
         }
         return link;
--- src/main/java/com/soundbrew/soundbrew/dto/ResponseDTO.java
+++ src/main/java/com/soundbrew/soundbrew/dto/ResponseDTO.java
@@ -5,7 +5,6 @@ import lombok.*;
 
 import java.util.List;
 
-<<<<<<< HEAD
 @Getter
 @Builder
 @AllArgsConstructor
@@ -15,17 +14,6 @@ import java.util.List;
 public class ResponseDTO<E> {
     private E dto;
     private List<E> dtoList;    // 데이터 목록
-
-=======
-@Data
-@Builder
-@RequiredArgsConstructor
-@AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_DEFAULT) // null 값인 필드는 제외
-public class ResponseDTO<E> {
-    private List<E> dtoList;// 데이터 목록
-    private E dto; //단수일때 쓰는것.
->>>>>>> feature/kyoung
     private int page;       // 현재 페이지
     private int size;       // 페이지 크기
     private int total;      // 전체 데이터 개수
@@ -33,7 +21,6 @@ public class ResponseDTO<E> {
     private int end;        // 끝 페이지
     private boolean prev;   // 이전 페이지 존재 여부
     private boolean next;   // 다음 페이지 존재 여부
-<<<<<<< HEAD
     private String keyword; // 검색 키워드
     private String message;
 
@@ -100,36 +87,3 @@ public class ResponseDTO<E> {
 
 
 }
-=======
-    private String keyword1; // 검색 키워드
-    private String message;
-
-    @Builder(builderMethodName = "withMessage")
-    public ResponseDTO(String message){
-        this.message = message;
-    }
-
-    @Builder(builderMethodName = "withSingleData")
-    public ResponseDTO(E dto){
-        this.dto = dto;
-    }
-
-    @Builder(builderMethodName = "withAll")
-    public ResponseDTO(RequestDTO requestDTO, List<E> dtoList, int total){
-        if(total <= 0) return;
-        this.keyword1 = requestDTO.getKeyword();
-        this.page = requestDTO.getPage();
-        this.size = requestDTO.getSize();
-        this.total = total;
-        this.dtoList = dtoList;
-        this.end = (int)(Math.ceil(this.page/10.0))*10; // 화면에서 마지막 번호
-        this.start = this.end - 9; // 화면에서 시작 번호
-        int last = (int)(Math.ceil(total/(double)size));
-        this.end = end > last ? last : end;
-        this.prev = this.page > 1;
-//        this.prev = this.start > 1;
-//        this.next = total > this.end * this.size;
-        this.next = this.page < Math.ceil((double) total / this.size);
-    }
-}
->>>>>>> feature/kyoung
