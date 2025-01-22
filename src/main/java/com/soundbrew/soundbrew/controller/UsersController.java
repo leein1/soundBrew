b04999ed1f5c9110bb806f61d8f260ba5be39f70
leein1 <inwon.private@gmail.com>
2025-01-22 19:24:35 +0900

    no message

--- src/main/java/com/soundbrew/soundbrew/controller/UsersController.java
+++ src/main/java/com/soundbrew/soundbrew/controller/UsersController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsersController {
 
     private final UserService userService;
+
+    @GetMapping("")
+    public ResponseEntity<ResponseDTO<List<UserDTO>>> getUsers() {
+
+        return null;
+    }
+
 
 //    @ApiOperation(value = "users POST", notes = "POST 방식으로 회원 등록")
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -41,6 +47,8 @@ public class UsersController {
         }
 
     }
+
+
 
 
 
