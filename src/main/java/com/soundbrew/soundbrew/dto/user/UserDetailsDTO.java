c9392c2d7cbff080c634801918e3c52be6a7f7bb
leein1 <inwon.private@gmail.com>
2025-02-05 14:02:40 +0900

    수정 - UserDetailsDTO 생성자 파라미터 이름 수정

--- src/main/java/com/soundbrew/soundbrew/dto/user/UserDetailsDTO.java
+++ src/main/java/com/soundbrew/soundbrew/dto/user/UserDetailsDTO.java
@@ -23,11 +23,11 @@ public class UserDetailsDTO implements UserDetails {
 
     private List<GrantedAuthority> authorities;
 
-    public UserDetailsDTO(String username, int userId, String nickname, List<GrantedAuthority> roles){
+    public UserDetailsDTO(String email, int userId, String nickname, List<GrantedAuthority> roles){
 
         // 생성자 내부에서 UserDTO를 직접 초기화
         this.userDTO = new UserDTO();
-        this.userDTO.email = username;
+        this.userDTO.email = email;
         this.userDTO.userId = userId;
         this.userDTO.nickname = nickname;
         this.authorities = roles;
@@ -67,8 +67,8 @@ public class UserDetailsDTO implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-//        return userDTO.isEmailVerified();
-        return true;
+        return userDTO.isEmailVerified();
+//        return true;
     }
 
 
