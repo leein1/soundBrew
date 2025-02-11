6d0b165916c98346752d9722461df5285c73a53b
leein1 <inwon.private@gmail.com>
2025-02-11 16:00:30 +0900

    수정 - login.js 비동기 요청부분 수정

--- src/main/resources/static/js/security/login.js
+++ src/main/resources/static/js/security/login.js
@@ -7,37 +7,50 @@ export function getLoginFormProcess() {
 
         const username = document.getElementById("email").value;
         const password = document.getElementById("password").value;
-        const data = { username, password };
+        const userInput = { username, password };
 
         try {
-            const response = await generateTokenFromLogin(data);
-            alert(response.response.status.toString());
-            if(response.response && response.response.status ===200){
 
-                window.location.href="/sounds/tracks";
-            }
+            const response = await axios.post("http://localhost:8080/generateToken", userInput);
+            console.log("generateTokenFromLogin - " + response.status); //확인용
+
+            const accessToken = response.data.accessToken;
+            const refreshToken = response.data.refreshToken;
+
+            localStorage.setItem("accessToken", accessToken);
+            localStorage.setItem("refreshToken", refreshToken);
+
+            // alert(response.data.redirectUrl);
+            window.location.href = response.data.redirectUrl;
+
         } catch (error) {
-            console.error("Error during login or resource access:", error);
-            alert("로그인 또는 리소스 요청 중 문제가 발생했습니다.");
+
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+
+            alert(error.response.data.message);
+            window.location.href = error.response.data.redirectUrl;
         }
     });
 }
 
-export async function generateTokenFromLogin(userInput) {
-    try {
-        const response = await axios.post("http://localhost:8080/generateToken", userInput);
-        // const response = await axiosPost({endpoint:'/generateToken',body:userInput});
-
-        const accessToken = response.data.accessToken;
-        const refreshToken = response.data.refreshToken;
-        const redirectUrl = response.data.redirectUrl;
-
-        localStorage.setItem("accessToken", accessToken);
-        localStorage.setItem("refreshToken", refreshToken);
-
-
-        return { accessToken, refreshToken, redirectUrl, response };
-    } catch (err) {
-        console.error("로그인 과정 중 실패 :", err);
-    }
-}
+// export async function generateTokenFromLogin(userInput) {
+//     try {
+//         const response = await axios.post("http://localhost:8080/generateToken", userInput);
+//         // const response = await axiosPost({endpoint:'/generateToken',body:userInput});
+//
+//         console.log("generateTokenFromLogin - " + response.status);
+//
+//         const accessToken = response.data.accessToken;
+//         const refreshToken = response.data.refreshToken;
+//         const redirectUrl = response.data.redirectUrl;
+//
+//         localStorage.setItem("accessToken", accessToken);
+//         localStorage.setItem("refreshToken", refreshToken);
+//
+//
+//         return { accessToken, refreshToken, redirectUrl, response };
+//     } catch (err) {
+//         console.error("로그인 과정 중 실패 :", err);
+//     }
+// }
