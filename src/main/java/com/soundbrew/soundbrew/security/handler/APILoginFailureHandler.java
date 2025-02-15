195ba2919a4862e24f07780435702ba8bce00c62
leein1 <inwon.private@gmail.com>
2025-02-15 13:23:51 +0900

    수정 - APILoginFailureHandler 예외 종류에 따른 조건문 + 응답 추가

--- src/main/java/com/soundbrew/soundbrew/security/handler/APILoginFailureHandler.java
+++ src/main/java/com/soundbrew/soundbrew/security/handler/APILoginFailureHandler.java
@@ -3,6 +3,7 @@ package com.soundbrew.soundbrew.security.handler;
 import com.google.gson.Gson;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -25,11 +26,44 @@ public class APILoginFailureHandler implements AuthenticationFailureHandler {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
 
+
+
+        String message = "";
+        String redirectUrl = "";
+
+        if(exception instanceof LockedException){
+
+            message = "계정이 잠겨 있습니다.";
+
+        } else if(exception instanceof DisabledException){
+
+            message = "계정이 비활성화되었습니다.";
+
+        } else if(exception instanceof AccountExpiredException){
+
+            message = "계정이 만료되었습니다.";
+
+        } else if(exception instanceof CredentialsExpiredException){
+
+            message = "비밀번호가 만료되었습니다. 비밀번호를 변경해주세요.";
+            redirectUrl = "비밀번호 변경 url";
+
+        } else if(exception instanceof BadCredentialsException){
+
+            message = "아이디 또는 비밀번호가 올바르지 않습니다.";
+
+        } else if(exception instanceof AuthenticationServiceException){
+
+            message = "현재 인증 서비스에 문제가 있습니다. 잠시후 시도하시거나 문의해주세요.";
+
+        }
+
         // 에러 메시지를 JSON 형태로 응답
         Map<String,String> keyMap = Map.of(
-            "message", "로그인 실패",
-            "redirectUrl", "/login"
+                "message", message,
+                "redirectUrl", redirectUrl
         );
+
 
         Gson gson = new Gson();
 
