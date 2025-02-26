99365ef8f952569ee7b3913ad494d317110c5aec
leein1 <inwon.private@gmail.com>
2025-02-26 19:07:47 +0900

    수정 - Optional<Role> 반환 findById()생성

--- src/main/java/com/soundbrew/soundbrew/repository/role/RoleRepository.java
+++ src/main/java/com/soundbrew/soundbrew/repository/role/RoleRepository.java
@@ -4,6 +4,9 @@ package com.soundbrew.soundbrew.repository.role;
 import com.soundbrew.soundbrew.domain.user.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
+    Optional<Role> findById(int id);
 }
